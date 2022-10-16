023e58970a8831b3cabe4d7b7cae2cb49e0464bd
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-10-16 14:44:08 -0300

    feat: created Heading and Text components

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, BoxProps } from '@cr-ui/react'
+import { Box, BoxProps, Text } from '@cr-ui/react'
 
 export default {
   title: 'Surfaces/Box',
@@ -7,7 +7,7 @@ export default {
   args: {
     children: (
       <>
-        <span>Elemento Box</span>
+        <Text>Elemento Box</Text>
       </>
     ),
   },
